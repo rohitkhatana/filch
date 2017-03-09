f3ccda051b9f59dba54a9a017582bb0040492066
Rohit Khatana <rohitkhatana.khatana@gmail.com>
2017-03-09 17:53:14 +0530

    added content type find type implementation instead of using extension, using native file command

---
+++ file_type.py
@@ -0,0 +1,19 @@
+import subprocess
+
+
+class FileType(object):
+
+
+    def __init__(self, path):
+        self.file_type = self.__file_type(path)
+
+    def __file_type(self, path):
+        return subprocess.check_output(['file', path]).lower()
+
+    def find(self):
+        if 'image' in self.file_type:
+            return 'image'
+        elif 'pdf' in self.file_type:
+            return 'pdf'
+        elif 'zip' in self.file_type:
+            return 'zip'
